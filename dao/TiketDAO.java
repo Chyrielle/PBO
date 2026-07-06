36ac92635a3759d7ede9235d2f1a36141c871475
Calmantha <khususmantha@gmail.com>
2026-07-06 12:40:14 +0800

    Update TiketDAO.java

--- dao/TiketDAO.java
+++ dao/TiketDAO.java
@@ -1,6 +1,5 @@
 package dao;
 
-
 import database.DatabaseConnection;
 import model.Tiket;
 
@@ -11,4 +10,33 @@ import java.sql.SQLException;
 
 public class TiketDAO {
 
+    public boolean tambah(Tiket tiket) {
+
+        String sql =
+                "INSERT INTO tiket (nama_tiket, harga, stok_tiket) VALUES (?, ?, ?)";
+
+        try (
+                Connection conn = DatabaseConnection.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)
+        ) {
+
+            stmt.setString(1, tiket.getNamaTiket());
+            stmt.setDouble(2, tiket.getHarga());
+            stmt.setInt(3, tiket.getStokTiket());
+
+            stmt.executeUpdate();
+
+            return true;
+
+        } catch (SQLException e) {
+
+            System.out.println(
+                    "Error tambah tiket : "
+                    + e.getMessage()
+            );
+
+            return false;
+        }
+    }
+}
 }
