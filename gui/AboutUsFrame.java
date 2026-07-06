72dd3c97555fb9c31fac7654e4782e751e10bffa
Calmantha <khususmantha@gmail.com>
2026-07-06 19:09:25 +0800

    Update AboutUsFrame.java

--- gui/AboutUsFrame.java
+++ gui/AboutUsFrame.java
@@ -8,22 +8,95 @@ public class AboutUsFrame extends JFrame {
     public AboutUsFrame() {
 
         setTitle("About Us - NextFly");
-        setSize(600, 400);
+        setSize(900, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(5, 1));
+        JPanel mainPanel = new JPanel(new BorderLayout());
 
-        panel.add(new JLabel("NEXTFLY", SwingConstants.CENTER));
+        JLabel title = new JLabel(
+                "NEXTFLY DEVELOPMENT TEAM",
+                SwingConstants.CENTER
+        );
 
-        panel.add(new JLabel("Arya", SwingConstants.CENTER));
-        panel.add(new JLabel("Yoga", SwingConstants.CENTER));
-        panel.add(new JLabel("Ajuz", SwingConstants.CENTER));
-        panel.add(new JLabel("Nama Anda", SwingConstants.CENTER));
+        title.setFont(new Font("Arial", Font.BOLD, 22));
 
-        add(panel);
+        mainPanel.add(title, BorderLayout.NORTH);
+
+        JPanel anggotaPanel = new JPanel(
+                new GridLayout(2, 2, 20, 20)
+        );
+
+        anggotaPanel.add(createMemberPanel(
+                "images/yoga.jpg",
+                "Made Arya Dharma Putra Mahothama",
+                "250030020"
+        ));
+
+        anggotaPanel.add(createMemberPanel(
+                "images/yoga.jpg",
+                "I Gusti Agung Bramantha Prana Citra J.",
+                "250030061"
+        ));
+
+        anggotaPanel.add(createMemberPanel(
+                "images/yoga.jpg",
+                "I Putu Yoga Pratama",
+                "250030062"
+        ));
+
+        anggotaPanel.add(createMemberPanel(
+                "images/yoga.jpg",
+                "Gede Agung Bagus Aryadinatha",
+                "250030094"
+        ));
+
+        mainPanel.add(anggotaPanel, BorderLayout.CENTER);
+
+        add(mainPanel);
 
         setVisible(true);
     }
+
+    private JPanel createMemberPanel(
+            String imagePath,
+            String nama,
+            String nim
+    ) {
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(
+                panel,
+                BoxLayout.Y_AXIS
+        ));
+
+        ImageIcon icon = new ImageIcon(imagePath);
+
+        Image image = icon.getImage().getScaledInstance(
+                120,
+                120,
+                Image.SCALE_SMOOTH
+        );
+
+        JLabel foto = new JLabel(
+                new ImageIcon(image)
+        );
+
+        foto.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JLabel namaLabel = new JLabel(nama);
+        namaLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JLabel nimLabel = new JLabel("NIM: " + nim);
+        nimLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        panel.add(Box.createVerticalStrut(10));
+        panel.add(foto);
+        panel.add(Box.createVerticalStrut(10));
+        panel.add(namaLabel);
+        panel.add(nimLabel);
+
+        return panel;
+    }
+}
 }
