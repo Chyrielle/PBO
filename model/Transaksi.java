4efac351b63aaf0c152b6a3c330ea5cdab08f38b
Calmantha <khususmantha@gmail.com>
2026-07-06 14:58:14 +0800

    Update Transaksi.java

--- model/Transaksi.java
+++ model/Transaksi.java
@@ -1,1 +1,64 @@
 
+package model;
+
+public class Transaksi {
+
+    private int id;
+    private int tiketId;
+    private int jumlah;
+    private double totalHarga;
+    private String tanggal;
+
+    public Transaksi() {
+    }
+
+    public Transaksi(int id, int tiketId, int jumlah,
+                     double totalHarga, String tanggal) {
+
+        this.id = id;
+        this.tiketId = tiketId;
+        this.jumlah = jumlah;
+        this.totalHarga = totalHarga;
+        this.tanggal = tanggal;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTiketId() {
+        return tiketId;
+    }
+
+    public void setTiketId(int tiketId) {
+        this.tiketId = tiketId;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public double getTotalHarga() {
+        return totalHarga;
+    }
+
+    public void setTotalHarga(double totalHarga) {
+        this.totalHarga = totalHarga;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+}
