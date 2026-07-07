c503e5a9a8fba892823f61d53623d955656786b6
Chyrielle <steveeeeg13@gmail.com>
2026-07-07 11:02:05 +0800

    Update App.java

--- app/App.java
+++ app/App.java
@@ -1,5 +1,16 @@
+package app;
+
+import gui.LoginFrame;
+
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) {
+        }
 
+        SwingUtilities.invokeLater(() -> new LoginFrame().setVisible(true));
     }
 }
